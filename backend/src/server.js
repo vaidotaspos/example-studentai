c83001858f1887dea552d5f77b790664a5cd572b
Vaidotas Poskus <vaidotas@bitcat.dev>
2024-01-24 11:31:35 +0200

    Paturbintas serveris

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,24 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const {mainErrroHandler} = require("./middleware");
+const APIError = require("./apiError/ApiError");
 
 const app = express();
 
 const port = process.env.PORT || 3000
+
+// Include Middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+app.get('/', (req, res) => {
+   res.json({
+      message: 'Server Is Running'
+   });
+});
 
 app.listen(port, () => {
    console.log(`Server is listening on port ${port}`)

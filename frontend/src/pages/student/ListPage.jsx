35c84e058e70eea8ed4ad1a966e420d594addb7e
Vaidotas Poskus <vaidotas@nexinno.tech>
2024-02-01 10:25:21 +0200

    Frontend: added student list filter

--- frontend/src/pages/student/ListPage.jsx
+++ frontend/src/pages/student/ListPage.jsx
@@ -4,8 +4,11 @@ import {Link, useNavigate} from "react-router-dom"
 import axios from "axios";
 import toast from "react-hot-toast";
 import {useAuthContext} from "../../store/AuthCtxProvider.jsx";
+import {useMemo, useState} from "react";
 
 export default function ListPage() {
+    const [filterValue, setFilterValue] = useState('')
+
     const [studentList, setStudentList] = useApiData(`${baseApiUrl}students`);
 
     const {isUserAdmin, isUserLoggedIn, token} = useAuthContext();
@@ -29,10 +32,27 @@ export default function ListPage() {
             })
     }
 
+    const filteredStudents = useMemo(() => {
+       return studentList.filter(student => student.firstname.toLowerCase().includes(filterValue.toLowerCase()) || student.lastname.toLowerCase().includes(filterValue.toLowerCase()) || student.email.toLowerCase().includes(filterValue.toLowerCase()))
+    }, [studentList, filterValue]);
+
+    const handleFilterChange = event => {
+        setFilterValue(event.target.value);
+    }
+
     return (
         <div className='container mx-auto p-4'>
             <h1 className='text-4xl'>Studentu sarasas</h1>
 
+            <div className="mt-5">
+                <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    type="text"
+                    onChange={handleFilterChange}
+                    value={filterValue}
+                    placeholder="Pasieška"
+                />
+            </div>
             <div className='mt-5'>
                 <table className="min-w-full table-auto">
                     <thead className="bg-gray-600 text-white">
@@ -45,17 +65,17 @@ export default function ListPage() {
                     </tr>
                     </thead>
                     <tbody>
-                    {studentList.map((studentas) => (
-                        <tr key={studentas.id} className="bg-gray-200">
-                            <td className="border px-4 py-2">{studentas.id}</td>
-                            <td className="border px-4 py-2">{studentas.firstname}</td>
-                            <td className="border px-4 py-2">{studentas.lastname}</td>
-                            <td className="border px-4 py-2">{studentas.email}</td>
+                    {filteredStudents.map((student) => (
+                        <tr key={student.id} className="bg-gray-200">
+                            <td className="border px-4 py-2">{student.id}</td>
+                            <td className="border px-4 py-2">{student.firstname}</td>
+                            <td className="border px-4 py-2">{student.lastname}</td>
+                            <td className="border px-4 py-2">{student.email}</td>
                             <td className="border px-4 py-2">
                                 { isUserLoggedIn && (
                                     <>
                                         <Link
-                                            to={`/edit-student/${studentas.id}`}
+                                            to={`/edit-student/${student.id}`}
                                             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                                         >
                                             Redaguoti
@@ -64,7 +84,7 @@ export default function ListPage() {
                                         { isUserAdmin && (
                                             <button
                                                 className="bg-red-500 hover:bg-red-400 text-white font-bold ml-2 py-2 px-4 rounded"
-                                                onClick={() => deleteStudent(studentas.id)}
+                                                onClick={() => deleteStudent(student.id)}
                                             >
                                                 Ištrinti
                                             </button>
